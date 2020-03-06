f120a82f9a439afd70d4a180be1e1cf7098ef5fa
dengdaiyemanren <315531514@qq.com>
2020-03-06 22:10:55 +0800

    chapter7Fragments

---
+++ 07_UIFragments/app/src/main/java/com/example/crimeactivity/Crime.java
@@ -0,0 +1,59 @@
+package com.example.crimeactivity;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+
+    public Crime()
+    {
+ 		mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+   
+
+    private UUID mId;
+    private Date mDate;
+
+
+    private String mTitle;
+
+    private boolean mSolved;
+
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID id) {
+        mId = id;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+
+
+}
